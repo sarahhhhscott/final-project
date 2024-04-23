0228b80ffbbce9cefbb37e1b6be5a95137b00d9b
sarahhhhscott <ses220007@utdallas.edu>
2024-04-23 18:55:25 -0500

    created window of game centered to the screen's dimensions. didnt realize i didnt create my file in the repository so it is here now

---
+++ final.py
@@ -0,0 +1,34 @@
+import tkinter
+import random
+
+ROWS = 25
+COLS = 25
+TITLE_SIZE = 25
+
+WINDOW_WIDTH = TITLE_SIZE * ROWS 
+WINDOW_HEIGHT = TITLE_SIZE * COLS
+
+
+#game window
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+canvas = tkinter.Canvas(window, bg = "black", width = WINDOW_WIDTH, height = WINDOW_HEIGHT, borderwidth = 0, highlightthickness = 0)
+canvas.pack()
+window.update()
+
+#center the window
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+window_x = int((screen_width/2) - (window_width/2))
+window_y = int((screen_height/2) - (window_height/2))
+#format "(w)x(h)+(x)+(y)"
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+
+
+window.mainloop()
