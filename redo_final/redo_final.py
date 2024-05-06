34b19bd0e9b32af352cc154e78e6bb5d721b6807
sarahhhhscott <158008403+sarahhhhscott@users.noreply.github.com>
2024-05-06 14:44:43 -0500

    Created redo_final.py. cannot seem to figure out check collision issue

---
+++ redo_final/redo_final.py
@@ -0,0 +1,101 @@
+import pygame, sys, random
+from pygame.math import Vector2
+
+
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
+        self.direction = Vector2(1,0)
+
+    def draw_snake(self):
+        for block in self.body:
+            #create a rect
+            for block in self.body:
+                x_pos = int(block.x * cell_size)
+                y_pos = int(block.y * cell_size)
+                block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            #draw the rectangle
+            pygame.draw.rect(screen,(250,10,5), block_rect)
+
+    def move_snake(self):
+        body_copy = self.body[:-1]
+        body_copy.insert(0,body_copy[0] + self.direction)
+        self.body = body_copy[:]
+
+class FRUIT:
+    def __init__(self):
+        #create an x and y position
+        self.randomize()
+        
+        #draw a square
+
+    def draw_fruit(self):
+        #create rectangle
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size)
+        #draw rectangle
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
+
+    def randomize(self):
+        self.x = random.randint(0,cell_number - 1)
+        self.y = random.randint(0,cell_number - 1)
+        self.pos = Vector2(self.x,self.y)
+
+class MAIN:
+    def __init__(self):
+        self.snake = SNAKE()
+        self.fruit = FRUIT()
+
+    def update(self):
+        self.snake.move_snake()
+
+    def draw_elements(self):
+        self.fruit.draw_fruit()
+        self.snake.draw_snake()
+
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            #reposition the fruit
+            self.fruit.randomize()
+            #add another block to the snake
+            
+
+pygame.init()
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+clock = pygame.time.Clock()
+#test_surface = pygame.Surface((100,200))
+#test_surface.fill((0,0,255))
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 150)
+#test_rect = test_surface.get_rect(center = (200,250))
+pygame.mixer.init()
+pygame.mixer.music.load('music.wav')
+pygame.mixer.music.play()
+
+main_game = MAIN()
+
+
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                main_game.snake.direction = Vector2(0,-1)
+            if event.key == pygame.K_RIGHT:
+                main_game.snake.direction = Vector2(1,0)
+            if event.key == pygame.K_DOWN:
+                main_game.snake.direction = Vector2(0,1)
+            if event.key == pygame.K_LEFT:
+                main_game.snake.direction = Vector2(-1,0)
+    screen.fill((0,0,10))
+    main_game.draw_elements()
+    pygame.display.update()
+    clock.tick(60)
